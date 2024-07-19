ca7b7e74a6608432db6dc1546288eb48744edc23
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-07-19 14:34:45 +0530

    Simple comments for understanding

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -5,12 +5,16 @@ const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US",{
     minimumFractionDigits: 0,
 })
 
+//in simple terms the function Intl.NumberFormat formats with numbers and stuffs
+//if 1000 is given as input it gives $1,000 as output
 export function formatCurrency(amount: number){
     return CURRENCY_FORMATTER.format(amount)
 }
 
+
 const NUMBER_FORMATTER = new Intl.NumberFormat("en-US")
 
+//if 1000 is given as input it gives 1,000 as output
 export function formatNumber(number: number){
     return NUMBER_FORMATTER.format(number)
 }
