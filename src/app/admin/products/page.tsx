448fa6a676c37e744c1752d01d663d357d8df0bb
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-07-24 20:30:19 +0530

    Doing some simple UI for admin page

---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,38 @@
+import { PageHeader } from "../_components/PageHeader";
+import { Button } from "@/components/ui/button";
+import { Table, TableHeader, TableRow, TableHead } from "@/components/ui/table";
+import Link from "next/link";
+
+export default function AdminProductPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+        <Button asChild>
+          <Link href="/admin/products/new">Add Product</Link>
+        </Button>
+      </div>
+      <ProductsTable />
+    </>
+  );
+}
+
+function ProductsTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-0">
+            <span className="sr-only">Available for Purchase</span>
+          </TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Price</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+    </Table>
+  );
+}
